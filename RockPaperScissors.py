2fed104bb49f2a28880f5571c3ba73557e4ab490
Veerakumar Ashokan <48096810+DigitalSapien@users.noreply.github.com>
2023-05-17 14:49:43 +0530

    initial commit

---
+++ RockPaperScissors.py
@@ -0,0 +1,39 @@
+# Basic Python Game "Rock, Paper and Scissors"
+
+import random
+
+def get_choices():
+    player_choice = input ("Enter a choice (rock, paper, scissors): ")
+    options = ["rock" , "paper" , "scissors"]
+    computer_choice = random.choice(options)
+    choices = {"player" : player_choice, "computer" : computer_choice}
+    return choices
+  
+def  check_win(player, computer):
+  print (f"Player chose {player}, Computer chose {computer}")
+  
+  if player == computer :                                                 
+        return "It's a tie!"
+    elif player == "rock":                                                  
+        if computer == "scissors":                                                          
+         return "Player Wins!"
+        else:
+         return "Computer Wins"
+    elif player == "paper":                                                              
+        if computer == "rock":                                                                                    
+         return "Player Wins!"
+        else:
+         return "Computer Wins"
+    elif player == "scissors":                                                              
+        if computer == "paper":                                                                                 
+         return "Player Wins!"
+        else:
+         return "Computer Wins"
+      
+choices = get_choices()
+result = check_win(choices["player"], choices["computer"] )               
+print (result)
+        
+  
+ 
+    
