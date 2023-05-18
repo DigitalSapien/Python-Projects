b00eb0dfd55e15a4d9014c7d36710ac772064dfb
Veerakumar Ashokan <48096810+DigitalSapien@users.noreply.github.com>
2023-05-18 13:56:12 +0530

    initial commit

--- PDF2Audio.py
+++ PDF2Audio.py
@@ -1,1 +1,13 @@
+import pyttsx3
+import PyPDF2
 
+book = open('A+.pdf','rb' )
+pdfReader = PyPDF2.PdfReader(book)
+pages = len(pdfReader.pages)
+print(pages)
+
+audio = pyttsx3.init()
+page = pdfReader.pages[31]
+text = page.extract_text()
+audio.say(text)
+audio.runAndWait()
