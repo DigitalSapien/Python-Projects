a56c70452c1df4c3b92342eebbc0bc75d59f8d4f
Veerakumar Ashokan <48096810+DigitalSapien@users.noreply.github.com>
2023-05-18 15:49:47 +0530

    initial commit

--- PDF2Audio.py
+++ PDF2Audio.py
@@ -1,3 +1,5 @@
+# PDF --> Audio  
+
 import pyttsx3
 import PyPDF2
 
@@ -11,3 +13,6 @@ page = pdfReader.pages[31]
 text = page.extract_text()
 audio.say(text)
 audio.runAndWait()
+
+# !! Outputs a mp3 File, Saves it to the Downloads folder.
+
